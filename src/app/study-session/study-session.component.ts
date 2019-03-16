9366ab1894566cc437e262af1148880df6cc07f0
iarigby <marvinzem@gmail.com>
2019-03-17 00:29:44 +0100

    added some comments

--- src/app/study-session/study-session.component.ts
+++ src/app/study-session/study-session.component.ts
@@ -25,6 +25,7 @@ export class StudySessionComponent implements OnInit {
 
   pause_session() {
     this.session.segments[0].end = new Date()
+    // this.session.segments[0].end_position = 
   }
 
   resume_session() {
@@ -32,13 +33,19 @@ export class StudySessionComponent implements OnInit {
   }
 
   create_segment(pos: Position) {
+    // using unshift so that the latest segment is the first one
+    // and can easily be accessed with [0]
     this.session.segments.unshift(new Segment(new Date(), pos));
+    // TODO have manualy way of adding sessions or segments
+    // will select day for the session
+    // for segments just write time, start page, end page
   }
 
   calculate_duration() {
     const difference = this.session.segments.map(seg => {
       return (seg.end || new Date()).getTime() - seg.start.getTime()
-    }).reduce((a,b) => a+b)
+    }).reduce((a,b) => a+b) // because .sum is too much to ask from a language
+    // TODO for some reason the initial difference is 1 hour
     this.duration = new Date(difference)
   }
 }
